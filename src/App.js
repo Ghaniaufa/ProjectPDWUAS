11a9a91fd0f6e72b4d878a063ccd8dfefd907a88
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2023-07-27 19:00:22 +0700

    Create New Project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <body>
+          <h1>TURNAMENT VALORANT 2023</h1>
+          <p>
+            <strong>Turnamen Komunitas ONE Esports kembali dengan lebih banyak game dan negara! Temukan skuad Anda dan bersainglah untuk memperebutkan total hadiah mingguan US$225. Partisipasi gratis.</strong>
+            <a href="https://battlefy.com/oneesports/tournaments"><button class="centered-button">Daftar di sini</button></a>
+          </p>
+        </body>
+      </header>
+    </div>
+  );
+}
+
+export default App;
